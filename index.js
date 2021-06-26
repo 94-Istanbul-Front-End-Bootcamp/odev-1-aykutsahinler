23490e8b1a91a89176782713ec3e44165bd446b8
aykutsahinler <aykut-sahinler@hotmail.com>
2021-06-26 14:02:19 +0300

    add some changes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /* Selectors */
 
 let fullName = document.getElementById("nameSurname");
-let ageElement = document.getElementById("age");
+let age = document.getElementById("age");
 let showBtn = document.getElementById("showBtn");
 let linkedinUrl = document.getElementById("linkedinUrl");
 let githubUrl = document.getElementById("githubUrl");
-
+let cursor = document.getElementById("cursor");
+let card = document.getElementById("card");
 
 /* Add links to a href */
 
@@ -15,15 +16,36 @@ githubUrl.href = "https://github.com/aykutsahinler";
 /* Event Listeners */
 
 showBtn.addEventListener("click", showInfos);
+window.addEventListener("mousemove", move);
 
 /* Functions */
 
 function showInfos() {
-    let name = "Aykut";
-    let surname = "Şahinler";
-    let age = 23
-    fullName.innerHTML = name + " " + surname; 
-    ageElement.innerHTML = age;
-    showBtn.style.display = "none";
+  let name = "Aykut";
+  let surname = "Şahinler";
+  let ageNum = 23;
+  fullName.innerHTML = name + " " + surname;
+  age.innerHTML = ageNum;
+  showBtn.style.display = "none";
+  createBall();
 }
 
+function move(e) {
+  cursor.style.top = e.pageY + "px";
+  cursor.style.left = e.pageX + "px";
+  window.addEventListener("click", click);
+  function click(e) {
+    cursor.style.backgroundColor = "lightgrey";
+    setTimeout(function () {
+      cursor.style.backgroundColor = "transparent";
+    }, 200);
+  }
+}
+
+function createBall() {
+  let avatar = document.createElement("DIV");
+  for(let i = 0; i < 10; i++) {
+    card.appendChild(avatar);
+    avatar.classList.add("avatar");
+  }
+}
